c7d8a4757d243ce64f96646fb26fcf0341c224f1
Drew Williams <drewwilliams5280@gmail.com>
2020-08-01 13:37:26 -0600

    Sets up night_reader_test file

---
+++ test/night_reader_test.rb
@@ -0,0 +1,13 @@
+require "simplecov"
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+require './lib/night_reader'
+require './lib/file_reader'
+
+class NightReaderTest < Minitest::Test
+
+
+
+end
