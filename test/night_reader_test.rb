6af1e888b69d248fadcf50c1db8782268a7165ec
Drew Williams <drewwilliams5280@gmail.com>
2020-08-01 14:59:52 -0600

    Adds test setup for translate method

--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -37,10 +37,6 @@ class NightReaderTest < Minitest::Test
     assert_equal "z", night_reader.reader.braille_to_character_dictionary["0..000"]
   end
 
-  def test_it_can_translate_braille_character_from_file
-    ARGV.replace ["braille.txt", "original_message.txt"]
-    night_reader = NightReader.new
-  end
 
   def test_it_can_get_dictionary
     ARGV.replace ["message.txt", "braille.txt"]
@@ -51,5 +47,10 @@ class NightReaderTest < Minitest::Test
     assert_equal test_array, night_reader.reader.character_to_braille_dictionary.keys
   end
 
+  def test_it_can_translate_braille_character
+    ARGV.replace ["braille.txt", "original_message.txt"]
+    night_reader = NightReader.new
+    assert_equal "t", night_reader.translate_braille(".0000.")
+  end
 
 end
