1d4c77f7ac0e06144b80c4767d0a46cbb7d88b19
Drew Williams <drewwilliams5280@gmail.com>
2020-08-01 13:43:29 -0600

    Adds attribute test

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,7 +1,12 @@
+require './lib/file_reader'
+require 'csv'
+
 class NightReader
 
-
-
+  attr_reader :reader
+  def initialize
+    @reader = FileReader.new
+  end
 
 
 end
--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -8,9 +8,10 @@ require './lib/file_reader'
 
 class NightReaderTest < Minitest::Test
 
-  def test_it_exists
+  def test_it_exists_with_attributes
     night_reader = NightReader.new
     assert_instance_of NightReader, night_reader
+    assert_instance_of FileReader, night_reader.reader
   end
 
 
