873f04b9782c6553a4efbb211681e6d50b27ac09
Drew Williams <drewwilliams5280@gmail.com>
2020-08-01 13:38:24 -0600

    Sets up exists test

--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -8,6 +8,12 @@ require './lib/file_reader'
 
 class NightReaderTest < Minitest::Test
 
+  def test_it_exists
+    night_reader = NightReader.new
+    assert_instance_of NightReader, night_reader
+
+  end
+
 
 
 end
