5d395197ec26376f045a7b3f7e99ddeffffa0cba
Drew Williams <drewwilliams5280@gmail.com>
2020-08-02 11:08:13 -0600

    Refactors translate_braille_file method by adding translate_braille_line

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -14,41 +14,44 @@ class NightReader
     "Created '#{ARGV[1]}' containing #{translate_braille_file.size} characters"
   end
 
+
   def translate_braille_file
     english_characters = ""
     braille = @reader.read.delete("\n")
     if braille.size > 240
       (braille.size / 240).times do
         40.times do |num|
-          braille1 = braille[num * 2]
-          braille2 = braille[num * 2 + 1]
-          braille3 = braille[num * 2 + 80]
-          braille4 = braille[num * 2 + 81]
-          braille5 = braille[num * 2 + 160]
-          braille6 = braille[num * 2 + 161]
-          english_characters += @reader.braille_to_character_dictionary[braille1 + braille2 + braille3 + braille4 + braille5 + braille6]
+          english_characters += translate_braille_line(braille, num)
           if num == 39
             braille.slice!(0..239)
           end
         end
       end
     end
-    english_characters += translate_last_line(braille)
+    english_characters += translate_last_line(braille, braille.size / 6)
   end
 
-  def translate_last_line(braille)
+  def translate_braille_line(braille, num)
+    text = ""
+    braille1 = braille[num * 2]
+    braille2 = braille[num * 2 + 1]
+    braille3 = braille[num * 2 + 80]
+    braille4 = braille[num * 2 + 81]
+    braille5 = braille[num * 2 + 160]
+    braille6 = braille[num * 2 + 161]
+    text += @reader.braille_to_character_dictionary[braille1 + braille2 + braille3 + braille4 + braille5 + braille6]
+  end
+
+  def translate_last_line(braille, size)
     last_line_characters = ""
-    if braille.size > 0
-      size = braille.size / 6
-      size.times do |num|
-        braille1 = braille[num * 2]
-        braille2 = braille[num * 2 + 1]
-        braille3 = braille[num * 2 + (size * 2)]
-        braille4 = braille[num * 2 + (size * 2 + 1)]
-        braille5 = braille[num * 2 + (size * 2 * 2)]
-        braille6 = braille[num * 2 + (size * 2 * 2 + 1)]
-        last_line_characters += @reader.braille_to_character_dictionary[braille1 + braille2 + braille3 + braille4 + braille5 + braille6]
-      end
+    size.times do |num|
+      braille1 = braille[num * 2]
+      braille2 = braille[num * 2 + 1]
+      braille3 = braille[num * 2 + (size * 2)]
+      braille4 = braille[num * 2 + (size * 2 + 1)]
+      braille5 = braille[num * 2 + (size * 2 * 2)]
+      braille6 = braille[num * 2 + (size * 2 * 2 + 1)]
+      last_line_characters += @reader.braille_to_character_dictionary[braille1 + braille2 + braille3 + braille4 + braille5 + braille6]
     end
     last_line_characters
   end
