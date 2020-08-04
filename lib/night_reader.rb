5b1040d157bcac91ed800ab4dc587653110f358d
Drew Williams <drewwilliams5280@gmail.com>
2020-08-03 20:51:40 -0600

    Refactors translate_braille_file method

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -19,34 +19,33 @@ class NightReader
     braille = @reader.read.delete("\n")
     if braille.size > 240
       (braille.size / 240).times do
-        40.times do |num|
-          english_characters += translate_braille_character(braille, num)
-          if num == 39
-            braille.slice!(0..239)
-          end
-        end
+        english_characters += translate_full_line(braille)
+        braille.slice!(0..239)
       end
     end
     english_characters += translate_last_line(braille, braille.size / 6)
   end
 
-  def translate_braille_character(braille, character)
-    text = ""
-    pair1 = braille[character * 2] + braille[character * 2 + 1]
-    pair2 = braille[character * 2 + 80] + braille[character * 2 + 81]
-    pair3 = braille[character * 2 + 160] + braille[character * 2 + 161]
-    text += @reader.braille_to_character_dictionary[pair1 + pair2 + pair3]
+  def translate_full_line(braille)
+    full_line_text = ""
+    40.times do |num|
+      pair1 = braille[num * 2] + braille[num * 2 + 1]
+      pair2 = braille[num * 2 + 80] + braille[num * 2 + 81]
+      pair3 = braille[num * 2 + 160] + braille[num * 2 + 161]
+      full_line_text += @reader.braille_to_character_dictionary[pair1 + pair2 + pair3]
+    end
+    full_line_text
   end
 
   def translate_last_line(braille, size)
-    last_line_characters = ""
+    last_line_text = ""
     size.times do |num|
       pair1 = braille[num * 2] + braille[num * 2 + 1]
       pair2 = braille[num * 2 + (size * 2)] + braille[num * 2 + (size * 2 + 1)]
       pair3 = braille[num * 2 + (size * 2 * 2)] + braille[num * 2 + (size * 2 * 2 + 1)]
-      last_line_characters += @reader.braille_to_character_dictionary[pair1 + pair2 + pair3]
+      last_line_text += @reader.braille_to_character_dictionary[pair1 + pair2 + pair3]
     end
-    last_line_characters
+    last_line_text
   end
 
   def decode_file_to_text
